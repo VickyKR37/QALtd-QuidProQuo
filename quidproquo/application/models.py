c342520f4e52f2053013ee1139eb9bbcfe2c220f
Vicky Rai <vickyks@hotmail.com>
2022-04-04 11:35:12 +0000

    VK: created users class and loans class in models.py

--- quidproquo/application/models.py
+++ quidproquo/application/models.py
@@ -3,4 +3,23 @@ from flask_wtf import Flaskform
 from wtforms import StringField, SubmitField, BooleanField, IntegerField, SelectField
 from wtforms.validators import DataRequired, Length, ValidationError
 from wtforms_sqlalchemy.fields import QuerySelectField
+from wtforms.widgets import PasswordInput
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt(app)
+
+class Users(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    user_name = db.column(db.String(15))
+    password = db.Column(db.String(15), nullable=False)
+    property = db.Column(db.Integer)
+    cash = db.Column(db.Integer)
+    investments = db.Column(db.Integer)
+
+class Loans(db.Model):
+    loan_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    amount_borrowed = db.Column(db.Integer)
+    amount_paid = db.column(db.Integer)
+    lender_id = db.Column(db.String(20))
 
